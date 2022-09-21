408abef0a4c65013e6b14f42c001a21ef958bfa0
suzz-in <o_o_su_jin_@naver.com>
2022-09-21 18:32:49 +0900

    Feat: 이미지 업로드 수정

--- src/components/write/WriteContent.jsx
+++ src/components/write/WriteContent.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
-const WriteContent = () => {
-  return <Content></Content>;
+const WriteContent = ({ setContent, setImage, Image }) => {
+  return (
+    <Content>
+      <input type="file" accept="image/*" />
+    </Content>
+  );
 };
 export default WriteContent;
 
--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Write from "../components/write/Write";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const WritePage = () => {
   const [title, setTitle] = useState("");
@@ -11,6 +12,8 @@ const WritePage = () => {
   const [time, setTime] = useState("");
   //이미지랑 같이 보낼려면 ! -> 마크다운형식으로 고쳐보자
   const [content, setContent] = useState("");
+
+  const navigate = useNavigate();
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
@@ -26,6 +29,7 @@ const WritePage = () => {
       "https://5bba5794-6cae-4b98-83a1-edd9ebc9483a.mock.pstmn.io/api/auth/post",
       data
     );
+    navigate("/");
   };
   return (
     <div>
