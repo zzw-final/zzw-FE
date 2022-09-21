401a080de2f80a5f2c4265b7a8b98bce3da41948
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-09-21 17:44:41 +0900

    Feat: kakaoLogin, join

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+// import Logo from "../components/common/Logo";
+import GoogleLogin from "../components/login/google/GoogleLogin";
+import KakaoLogin from "../components/login/kakao/KakaoLogin";
 
-function LoginPage() {
-  return <div>LoginPage</div>;
-}
+const LoginPage = () => {
+  return (
+    <div>
+      {/* <Logo /> */}
+      <LoginText>로그인을 해주세요</LoginText>
+      <LoginSelect>
+        <KakaoLogin />
+        <GoogleLogin />
+      </LoginSelect>
+    </div>
+  );
+};
+
+const LoginText = styled.div`
+  margin: auto;
+  text-align: center;
+  background-color: aqua;
+`;
+
+const LoginSelect = styled.div`
+  text-align: center;
+`;
 
 export default LoginPage;
