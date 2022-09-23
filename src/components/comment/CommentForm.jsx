9895cc266121e7cb39e9e61067f647cde803391a
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-09-23 15:57:28 +0900

    Feat: comment CRUD 작업 및 px -> rem 으로 수정

--- src/components/comment/CommentForm.jsx
+++ src/components/comment/CommentForm.jsx
@@ -1,51 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useDispatch } from "react-redux";
 import { postComment } from "../../redux/modules/commentSlice";
+import { useCookies } from "react-cookie";
 
 const CommentForm = ({ postId }) => {
-  const [comment, setComment] = useState("");
+  const [cookies] = useCookies(["loginProfile"]);
   const dispatch = useDispatch();
+  const commentRef = useRef("");
+  const loginProfile = cookies.loginProfile;
 
-  // 로그인 유저 정보 세션으로부터 받기
-  const commentId = Math.random();
-  const img =
-    "https://i.pinimg.com/474x/08/c6/d8/08c6d89c4049d7063c0b5b6167cbd566.jpg";
-  const nickname = "moon";
-  const grade = "징징이";
-
-  console.log("comment :>> ", comment);
-
-  const sendData = {
-    postId: postId,
-    comment: comment,
-    img: img,
-    nickname: nickname,
-    grade: grade,
-    createdAt: Date.now(),
-  };
+  useEffect(() => {
+    commentRef.current.focus();
+  }, []);
 
   const post = () => {
+    const sendData = {
+      postId: postId,
+      comment: commentRef.current.value,
+      profile: loginProfile,
+    };
     dispatch(postComment(sendData));
-    setComment("");
+    commentRef.current.value = "";
   };
 
-  const onChange = (event) => {
-    return setComment(event.target.value);
-  };
+  useEffect(() => {
+    commentRef.current.addEventListener("keypress", logKey);
+    function logKey(event) {
+      if (event.code === "Enter") {
+        post();
+      }
+    }
+  }, []);
 
   return (
     <FormContainer>
-      <TextField
-        id="outlined-start-adornment"
-        name="comment"
-        value={comment}
-        fullWidth
-        onChange={onChange}
-        size="small"
-      />
+      <CommentInput ref={commentRef} />
       <Button variant="outlined" sx={{ p: 0, ml: 1 }} onClick={post}>
         댓글 달기
       </Button>
@@ -55,7 +47,12 @@ const CommentForm = ({ postId }) => {
 
 const FormContainer = styled.div`
   display: flex;
-  margin: 0.4em;
+  margin: 0.4rem;
+`;
+
+const CommentInput = styled.input`
+  width: 100%;
+  height: 4vh;
 `;
 
 export default CommentForm;
