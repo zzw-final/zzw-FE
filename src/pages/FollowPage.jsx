94915b28310202c13e8a7d0a24d41e125732ace2
hyein <poptarts@kakao.com>
2022-10-04 20:12:51 +0900

    !HOTFIX: 다른 유저 리스트에서 mutate 발생시 내 리스트 캐시 무효화

--- src/pages/FollowPage.jsx
+++ src/pages/FollowPage.jsx
@@ -34,13 +34,13 @@ const FollowPage = () => {
     }
   };
 
-  const { data: followList } = useQuery(["follow", id], fetchFollow, {
+  const { data: followList } = useQuery(["follow", id ? id : "0"], fetchFollow, {
     enabled: !click, // 팔로우 진입시에만 받아옴
     cacheTime: 30 * 60 * 1000, // 캐시 30분 유지
     staleTime: Infinity, // 항상 신선한 데이터로 취급
     select: (data) => data.data.data, // 요청 성공시 데이터 가공
   });
-  const { data: followerList } = useQuery(["follower", id], fetchFollower, {
+  const { data: followerList } = useQuery(["follower", id ? id : "0"], fetchFollower, {
     enabled: !!click, // 팔로워 진입시에만 받아옴
     cacheTime: 30 * 60 * 1000, // 캐시 30분 유지
     staleTime: Infinity, // 항상 신선한 데이터로 취급
@@ -68,16 +68,22 @@ const FollowPage = () => {
   };
 
   // mutate(팔로우/언팔로우) 성공시 해당 키값의 캐시 무효화
+  // 다른 User의 리스트에서 mutate가 일어났다 -> 내가 누군가를 팔로우하거나 언팔로우했음을 의미
+  // 따라서 내 팔로우 캐시도 무효화 시킨다.
   const { mutate } = useMutation(followHandler, {
     onSuccess: () => {
       if (!click) {
         queryClient.invalidateQueries(["follow", id]);
+        queryClient.invalidateQueries(["follow", "0"]);
       }
       if (!!click) {
         queryClient.invalidateQueries(["follower", id]);
+        queryClient.invalidateQueries(["follow", "0"]);
       }
     },
   });
+
+  console.log("click", click);
 
   return (
     <LayoutPage>
