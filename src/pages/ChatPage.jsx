4bc7fe918237c908eafc397f5272b4a716d25e8d
hyein <poptarts@kakao.com>
2022-10-10 16:38:53 +0900

    Design: 채팅방 Layout 디자인

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,22 +1,12 @@
-import { useNavigate } from "react-router-dom";
-import Chat from "../components/chat/Chat";
+import React from "react";
+import ChatLayout from "../components/chat/UI/ChatLayout";
 
-const ChatPage = () => {
-  const navigate = useNavigate();
+function ChatPage() {
   return (
-    <>
-      {/* 헤더 후에 수정필요 */}
-      <div
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        홈으로
-      </div>
-      {/* 채팅내용 불러오기(채팅 작성 footer식으로 빼도 됨) */}
-      <Chat />
-    </>
+    <ChatLayout>
+      <div>여기에 수진님이 메시지 넣으시면 되는 부분 ~</div>
+    </ChatLayout>
   );
-};
+}
 
 export default ChatPage;
