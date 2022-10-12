3a89b80ad4e1838e29734964481ce574a57d742a
suzz-in <o_o_su_jin_@naver.com>
2022-10-12 17:38:29 +0900

    Fix: dev pull 오류 수정

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -3,7 +3,7 @@ import * as StompJs from "@stomp/stompjs";
 import { getCookie } from "../util/cookie";
 import useInput from "../hooks/useInput";
 import ChatLayout from "../components/chat/ChatLayout";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import SendMsg from "../components/chat/SendMsg";
 import GetMsg from "../components/chat/GetMsg";
 import { instance } from "../api/request";
@@ -12,7 +12,7 @@ import { useLocation } from "react-router-dom";
 function ChatPage() {
   const client = useRef({});
   const { roomId } = useParams();
-  const [msg, msgHandler] = useInput();
+  const [msg, msgHandler, setMsg] = useInput();
   const [messages, setMessages] = useState([{}]);
   const { state: location } = useLocation();
 
@@ -55,7 +55,6 @@ function ChatPage() {
       };
       console.log(newdata);
       await instance.put("/api/chat/newmessage", newdata);
-      navigate("/chatlist");
     };
     back();
     client.current.deactivate();
@@ -118,9 +117,7 @@ function ChatPage() {
       msgHandler={msgHandler}
       location={location}
     >
-      <div
-        style={{ margin: "50px 0px 50px 0px", width: "90%", height: "90%" }}
-      >
+      <div style={{ margin: "50px 0px 50px 0px", width: "90%", height: "90%" }}>
         {messages &&
           messages.map((mag, idx) =>
             loginNickname === messages[idx].sender ? (
@@ -132,7 +129,7 @@ function ChatPage() {
               />
             ) : (
               <GetMsg
-              location={location}
+                location={location}
                 messages={messages}
                 mag={mag}
                 idx={idx}
