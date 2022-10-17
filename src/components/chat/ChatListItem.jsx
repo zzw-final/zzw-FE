1be739e20842a08be95c1f16bc4fe578b049acc7
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-10-17 12:53:12 +0900

    Feat: 채팅방 리스트에서 채팅방 나가기 기능 삭제

--- src/components/chat/ChatListItem.jsx
+++ src/components/chat/ChatListItem.jsx
@@ -3,9 +3,8 @@ import styled from "styled-components";
 import Avatar from "@mui/material/Avatar";
 import { useNavigate } from "react-router-dom";
 
-const ChatListItem = ({ listItem, onSubmit }) => {
-  const { roomId, userId, profile, nickname, grade, message, isRead } =
-    listItem;
+const ChatListItem = ({ listItem }) => {
+  const { roomId, userId, profile, nickname, grade, message, isRead } = listItem;
 
   const navigate = useNavigate();
 
@@ -19,35 +18,15 @@ const ChatListItem = ({ listItem, onSubmit }) => {
     });
   };
 
-  const onDeleteChatRoom = (event) => {
-    event.stopPropagation();
-    if (
-      window.confirm(
-        "나가기를 누르면 채팅 데이터가 모두 삭제됩니다. 정말 나가시겠습니까?"
-      )
-    ) {
-      onSubmit(roomId);
-    } else {
-    }
-  };
-  // 안읽었으면 false,
-
   return (
     <ListItemContainer onClick={chatPage}>
-      <Avatar
-        alt="user_img"
-        src={profile}
-        sx={{ width: 48, height: 48, mr: 1 }}
-      />
+      <Avatar alt="user_img" src={profile} sx={{ width: 48, height: 48, mr: 1 }} />
       <ItemContent>
         <Nickname>
           {nickname} / {grade} {!isRead ? <IsReadAlert /> : ""}
         </Nickname>
         {message}
       </ItemContent>
-      <DeleteChatRoomBtn onClick={(e) => onDeleteChatRoom(e)}>
-        X
-      </DeleteChatRoomBtn>
     </ListItemContainer>
   );
 };
@@ -79,13 +58,6 @@ const ItemContent = styled.div`
   font-size: var(--font-small);
   color: var(--color-grey);
   width: 100%;
-`;
-
-const DeleteChatRoomBtn = styled.div`
-  font-size: var(--font-small);
-  position: absolute;
-  top: 1rem;
-  right: 0;
 `;
 
 export default ChatListItem;
