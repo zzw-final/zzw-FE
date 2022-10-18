6703a3f31edd0fc2c9e9d01ef11da275ad88f799
hyein <poptarts@kakao.com>
2022-10-19 00:17:52 +0900

    !HOTFIX: input type search 돋보기 겹침 현상 해결

--- src/components/followpage/FollowerList.jsx
+++ src/components/followpage/FollowerList.jsx
@@ -21,11 +21,7 @@ function FollowerList({ followerList, mutate }) {
         <span>
           <SearchIcon color="warning" />
         </span>
-        <Input
-          type="search"
-          placeholder="닉네임을 검색하세요."
-          onChange={searchInputHandler}
-        ></Input>
+        <Input placeholder="닉네임을 검색하세요." onChange={searchInputHandler}></Input>
       </SearchBox>
       <div style={{ height: "auto", marginBottom: "100px" }}>
         {searchFollowerList?.map((follow) => (
