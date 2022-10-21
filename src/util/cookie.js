3b8c11ad22af089a8cc1ebf70743e83ec201d509
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-10-21 14:15:57 +0900

    Feat: 쿠키 모두 삭제 함수 추가

--- src/util/cookie.js
+++ src/util/cookie.js
@@ -5,3 +5,8 @@ const cookies = new Cookies();
 export const setCookie = (name, value, option) => cookies.set(name, value, { ...option });
 export const getCookie = (name) => cookies.get(name);
 export const removeCookie = (name, option) => cookies.remove(name, { ...option });
+
+export function removeAllCookies() {
+  const cookies = document.cookie.split("; ");
+  for (let i in cookies) document.cookie = /^[^=]+/.exec(cookies[i])[0] + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+}
